d8eb82d4407835c6fd042533e910a62e83ea9935
harsha sri praneeth <praneethkonduru@Harshas-MacBook-Pro.local>
2021-12-14 00:37:54 -0500

    implemented dagger

---
+++ FetchRewardsApplication/app/src/main/java/com/example/fetchrewardsapplication/di/AppModule.kt
@@ -0,0 +1,30 @@
+package com.example.fetchrewardsapplication.di
+
+import com.example.fetchrewardsapplication.api.RetrofitAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(RetrofitAPI.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitAPI(retrofit: Retrofit): RetrofitAPI{
+        return retrofit.create(RetrofitAPI::class.java)
+    }
+}
